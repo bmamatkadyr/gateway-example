0578fd52acb2185e99c6e03926ead61d4b4f60ac
BeksultanDev <beksultancs@gmail.com>
2023-06-24 17:40:17 +0600

    shebang

--- src/main/java/beksultan/gatewayexample/GatewayExampleApplication.java
+++ src/main/java/beksultan/gatewayexample/GatewayExampleApplication.java
@@ -2,12 +2,59 @@ package beksultan.gatewayexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
+@EnableConfigurationProperties(UriConfiguration.class)
 public class GatewayExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayExampleApplication.class, args);
     }
 
+    @Bean
+    RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpUri = uriConfiguration.getHttpbin();
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("token", "noToken"))
+                        .uri(httpUri))
+                // circuit breaker
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("hi")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri)
+                )
+                .build();
+    }
+
+    // https://httpbin.org/get
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
+
+@ConfigurationProperties
+class UriConfiguration {
+    private String httpbin = "http://httpbin.org:80";
+
+    public String getHttpbin() {
+        return httpbin;
+    }
+
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
 }
